ed4fafbcde50fe61d815d973e3cdd408c7b8e5d2
Avram <aditaavram19@gmail.com>
2024-05-11 01:37:06 +0300

    Remove unused libraries, cleaning code in jest-setup and fix icon issue on iOS

--- jest-setup.js
+++ jest-setup.js
@@ -1,18 +1,14 @@
-import 'react-native-gesture-handler/jestSetup';
-import '@testing-library/jest-dom';  // Or import from 'jest-extended'
-import '@testing-library/jest-native/extend-expect'; // Ensure this is imported after jest-dom
+import '@testing-library/jest-dom';
+import '@testing-library/jest-native/extend-expect';
 
 import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock';
 jest.mock('@react-native-community/netinfo', () => {
-    return {
-      useNetInfo: jest.fn(() => ({
-        isConnected: true, // Set default connection status or customize as needed
-        // ... you can add more properties as needed for your testing
-      })), 
-    };
-  });
+  return {
+    useNetInfo: jest.fn(() => ({
+      isConnected: true,
+    })),
+  };
+});
 jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
 
 jest.mock('react-native-vector-icons/MaterialCommunityIcons', () => 'Icon');
-
-// Mock any other native modules or specific components here
